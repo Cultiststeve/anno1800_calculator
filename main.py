832903107c930f517529dc48b856bc4b1d349385
Unknown <steveage.ch@gmail.com>
2020-03-19 22:17:49 +0000

    more building types
    refactoried the displaying stuff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 
 POPULATION_RESIDENCES = {"farmers": 10, "workers": 20,
-                         "artisan": 30, "engineer": 40,
+                         "artisans": 30, "engineers": 40,
                          "explorers": -1, "technicians": -1}
 
 
@@ -32,59 +32,117 @@ FISHERY = ProductionBuilding(name="fishery",
 POTATO = ProductionBuilding(name="potato")
 SCHNAPPS = ProductionBuilding(name="schnapps",
                               consumers={"farmers": 600, "workers": 600,
-                                                    "explorers": 1333, "technicians": 1333},
+                                         "explorers": 1333, "technicians": 1333},
                               requires={"potato": 1})
 SHEEP = ProductionBuilding(name="sheep")
 KNITTERS = ProductionBuilding(name="knitters",
                               consumers={"farmers": 650, "workers": 650},
                               requires={"sheep": 1})
+PIG = ProductionBuilding(name="pig")
+SLAUGHTERS = ProductionBuilding(name="slaughters",
+                                consumers={"workers": 1000, "artisans": 750},
+                                requires={"pig": 1})
+GRAIN = ProductionBuilding(name="grain")
+FLOUR = ProductionBuilding(name="flour",
+                           requires={"grain": 2})
+BAKERY = ProductionBuilding(name="bakery",
+                            requires={"flour": .5},
+                            consumers={"workers": 1100, "artisans": 1650 / 2})
+RENDERING = ProductionBuilding(name="rendering",
+                               requires={"pig": 1})
+SOAP = ProductionBuilding(name="soap",
+                          requires={"rendering": 2},
+                          consumers={"workers": 4800, "artisans": 3600})
 
-PRODUCTION_BUILDINGS = {"fishery": FISHERY,
-                        "potato": POTATO,
-                        "schnapps": SCHNAPPS,
-                        "sheep": SHEEP,
-                        "knitters": KNITTERS}
+LUMBERJACK = ProductionBuilding(name="lumberjack")
+SAWMILL = ProductionBuilding(name="sawmill",
+                             requires={"lumberjack": 1})
+CLAY = ProductionBuilding(name="clay")
+BRICK = ProductionBuilding(name="brick",
+                           requires={"clay": .5})
+SAILMAKER = ProductionBuilding(name="sailmaker",
+                               requires={"sheep": 1})
+
+CONSUMABLES_BUILDINGS = {"fishery": FISHERY,
+                         "potato": POTATO,
+                         "schnapps": SCHNAPPS,
+                         "sheep": SHEEP,
+                         "knitters": KNITTERS,
+                         "pig": PIG,
+                         "slaughters": SLAUGHTERS,
+                         "grain": GRAIN,
+                         "flour": FLOUR,
+                         "bakery": BAKERY,
+                         "rendering": RENDERING,
+                         "soap": SOAP,
+                         "lumberjack": LUMBERJACK,
+                         "clay": CLAY}
+
+CONSTRUCTION_RES_BUILDING = {"sawmill": SAWMILL,
+                             "brick": BRICK,
+                             "sailmaker": SAILMAKER}
+
+ALL_BUILDINGS = {}
+ALL_BUILDINGS.update(CONSUMABLES_BUILDINGS)
+ALL_BUILDINGS.update(CONSTRUCTION_RES_BUILDING)
+
+
+POSSIBLE_FERTILITIES = ["hops", "wheat"]
+
 
 def calculate_requirements(building_name: str, building_number: int, existing_buildings: dict):
     """
-    
     Args:
         building_name: building to calculate suppliers for
         building_number: number of this building needed
         existing_buildings: dict to store results in
 
     Returns:
-
     """
-    assert building_name in PRODUCTION_BUILDINGS.keys()
+    assert building_name in CONSUMABLES_BUILDINGS.keys() or CONSTRUCTION_RES_BUILDING.keys()
     assert building_number > 0
 
-    for required_building in PRODUCTION_BUILDINGS[building_name].requires:
-        required_producer_num = building_number * PRODUCTION_BUILDINGS[building_name].requires[required_building]
+    for required_building in ALL_BUILDINGS[building_name].requires:
+        required_producer_num = building_number * ALL_BUILDINGS[building_name].requires[required_building]
         existing_buildings[required_building] += required_producer_num
         # If this producer has its own required producers, include them
-        if len(PRODUCTION_BUILDINGS[required_building].consumers) > 0:
-            calculate_requirements(building_name=building_name, building_number=required_producer_num, existing_buildings=existing_buildings)
+        for required_building_prechain in ALL_BUILDINGS[required_building].requires:
+            existing_buildings[required_building_prechain] += required_producer_num * ALL_BUILDINGS[required_building].requires[
+                required_building_prechain]
+            calculate_requirements(building_name=required_building_prechain, building_number=required_producer_num, existing_buildings=existing_buildings)
 
     return existing_buildings
 
 
-    
-
 class Island:
-    def __init__(self, name: str):
+    def __init__(self, name: str, fertility: set):
         self.name = name
+        for item in fertility:
+            assert item in POSSIBLE_FERTILITIES
+        self.fertility = fertility
+
         self.population = {}
         for pop in POPULATION_RESIDENCES:
             self.population[pop] = 0
+        self.constuction_res_building = {}
+        for building_type in CONSTRUCTION_RES_BUILDING:
+            self.constuction_res_building[building_type] = 0
 
     def required_production_buildings(self) -> dict:
+        """
+
+        Returns: actual building numbers required
+            (can be floats)
+
+        """
         results = {}
-        for building in PRODUCTION_BUILDINGS:
+        for building in CONSUMABLES_BUILDINGS:
+            results[building] = 0
+        for building in CONSTRUCTION_RES_BUILDING:
             results[building] = 0
 
-        # For each building
-        for building in PRODUCTION_BUILDINGS.values():
+        # For each consumable building
+        for building in CONSUMABLES_BUILDINGS.values():
             # for each consumer
             for consumer in building.consumers.keys():
                 if self.population[consumer] == 0:
@@ -96,11 +154,16 @@ class Island:
                     results[building.name] += buildings_required
                     # Calculate how many chain buildings needed to feed end of chain
                     calculate_requirements(building.name, buildings_required, results)
+        # for each construction resource building
+        for building in CONSTRUCTION_RES_BUILDING:
+            assert self.constuction_res_building[building] >= 0
+            if self.constuction_res_building[building] == 0:
+                continue
+            results[building] += self.constuction_res_building[building]
+            calculate_requirements(building, self.constuction_res_building[building], results)
 
-        # Round results up
-        for building_name in results:
-            results[building_name] = math.ceil(results[building_name])
         return results
+
 
     def required_residence_buildings(self) -> dict:
         results = {}
@@ -109,9 +172,28 @@ class Island:
 
         return results
 
+    def display_required(self):
+        res = self.required_production_buildings()
+        # Round results up
+        for building_name in res:
+            res[building_name] = math.ceil(res[building_name])
 
-ditchwater = Island(name="Ditchwater")
-ditchwater.population["farmers"] = 400
-ditchwater.population["workers"] = 200
-print(ditchwater.required_production_buildings())
-print(ditchwater.required_residence_buildings())
+        print("--- Required resource buildings ---")
+        for item in res:
+            print(f"{item} : {res[item]}")
+        res = self.required_residence_buildings()
+        print("--- Required residences ---")
+        for item in res:
+            print(f"{item} : {res[item]}")
+
+
+if __name__ == "__main__":
+    ditchwater = Island(name="Ditchwater", fertility={"wheat"})
+    ditchwater.population["farmers"] = 8 * 10 * 10
+    ditchwater.population["workers"] = 5 * 10 * 20
+    ditchwater.constuction_res_building["sawmill"] = 4
+    ditchwater.constuction_res_building["brick"] = 3
+    ditchwater.constuction_res_building["sailmaker"] = 1
+    ditchwater.display_required()
+    # print(ditchwater.required_production_buildings())
+    # print(ditchwater.required_residence_buildings())
--- tests/main_test.py
+++ tests/main_test.py
@@ -7,12 +7,12 @@ import main
 
 @pytest.fixture()
 def example_island():
-    example_island = main.Island(name="england")
+    example_island = main.Island(name="england", fertility=set())
     return example_island
 
 
 def test_pop_type_in_buildings():
-    for building in main.PRODUCTION_BUILDINGS.values():
+    for building in main.CONSUMABLES_BUILDINGS.values():
         for consumer in building.consumers:
             assert consumer in main.POPULATION_RESIDENCES
 
@@ -26,7 +26,7 @@ def test_add_to_pop(example_island):
 
 def test_all_buildings_in_required(example_island):
     required = example_island.required_production_buildings()
-    assert len(required) == len(main.PRODUCTION_BUILDINGS)
+    assert len(required) == len(main.CONSUMABLES_BUILDINGS) + len(main.CONSTRUCTION_RES_BUILDING)
 
 
 # [worker pop, required fishery]
@@ -34,7 +34,7 @@ def test_all_buildings_in_required(example_island):
 def test_fisher_numbers(example_island, example_numbers):
     example_island.population["farmers"] = example_numbers[0]
     required = example_island.required_production_buildings()
-    assert required["fishery"] == example_numbers[1]
+    assert math.ceil(required["fishery"]) == example_numbers[1]
 
 
 # [worker pop, required schnapps]
@@ -42,9 +42,7 @@ def test_fisher_numbers(example_island, example_numbers):
 def test_schnaps_numbers(example_island, example_numbers):
     example_island.population["farmers"] = example_numbers[0]
     required = example_island.required_production_buildings()
-    assert required
-    assert len(required) == len(main.PRODUCTION_BUILDINGS)
-    assert required["schnapps"] == example_numbers[1]
+    assert math.ceil(required["schnapps"]) == example_numbers[1]
 
 
 @pytest.mark.parametrize("population",
@@ -79,7 +77,7 @@ def test_residence_numbers(example_island, population):
 @pytest.mark.parametrize("example_numbers", [1, 1.1, 2, 4, 8, 32, 32.1, 65])
 def test_calculate_requirements_schnapps(example_numbers):
     existing = {}
-    for building_name in main.PRODUCTION_BUILDINGS:
+    for building_name in main.CONSUMABLES_BUILDINGS:
         existing[building_name] = 0
     res = main.calculate_requirements(building_name="schnapps", building_number=example_numbers, existing_buildings=existing)
     assert res["potato"] == example_numbers
@@ -90,4 +88,31 @@ def test_producer_chain_schnapps(example_island, example_farmer_number):
     example_island.population["farmers"] = example_farmer_number[0]
     assert example_island.population["workers"] == 0
     res = example_island.required_production_buildings()
-    assert res["potato"] == example_farmer_number[1]
+    assert math.ceil(res["potato"]) == example_farmer_number[1]
+
+
+# num workers, num bakerys, num flours,
+@pytest.mark.parametrize("example_numbers", [[1, 1, 1], [2199, 2, 1], [2200, 2, 1], [2201, 3, 2]])
+def test_bread_chain(example_island, example_numbers):
+    example_island.population["workers"] = example_numbers[0]
+    res = example_island.required_production_buildings()
+    assert math.ceil(res["bakery"]) == example_numbers[1]
+    assert math.ceil(res["grain"]) == example_numbers[1]
+    assert math.ceil(res["flour"]) == example_numbers[2]
+
+
+@pytest.mark.parametrize("example_numbers", [0, 1, 2, 3, 55])
+def test_sawmill(example_island, example_numbers):
+    example_island.constuction_res_building["sawmill"] = example_numbers
+    res = example_island.required_production_buildings()
+    assert res["lumberjack"] == example_numbers
+
+
+# number workers, number pig farms, number slaughterhouses, num soap factories
+@pytest.mark.parametrize("example_numbers", [[1, 1, 1, 1], [4800, 7, 5, 1]])
+def test_sausages_and_soap(example_island, example_numbers):
+    example_island.population["workers"] = example_numbers[0]
+    res = example_island.required_production_buildings()
+    assert math.ceil(res["pig"]) == example_numbers[1]
+    assert math.ceil(res["slaughters"]) == example_numbers[2]
+    assert math.ceil(res["soap"]) == example_numbers[3]
